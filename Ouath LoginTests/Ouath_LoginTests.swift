55bd0ba35ff853640be5a14c2b67e1d1e10a6087
vbharam@gmail.com <you@example.com>
2016-02-03 10:47:12 -0700

    Initial Commit

---
+++ Ouath LoginTests/Ouath_LoginTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ouath_LoginTests.swift
+//  Ouath LoginTests
+//
+//  Created by Vishal Bharam on 2/3/16.
+//  Copyright © 2016 codecoop. All rights reserved.
+//
+
+import XCTest
+@testable import Ouath_Login
+
+class Ouath_LoginTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
