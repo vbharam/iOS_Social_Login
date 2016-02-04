ff25b2fc14dd09d62e2e2dcd574c2e13a8b60b7d
vbharam@gmail.com <you@example.com>
2016-02-03 17:26:18 -0700

    Login layout

---
+++ Ouath Login/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Ouath Login
+//
+//  Created by Vishal Bharam on 2/3/16.
+//  Copyright © 2016 codecoop. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 157.0 / 255.0
+
---
+++ Ouath Login/UIView.swift
@@ -0,0 +1,19 @@
+//
+//  UIView.swift
+//  Ouath Login
+//
+//  Created by Vishal Bharam on 2/3/16.
+//  Copyright © 2016 codecoop. All rights reserved.
+//
+
+import Cocoa
+
+class UIView: NSView {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
